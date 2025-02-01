c1f501bb2bb1b50a3fb46971614c9830e6b70d4e
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-02-01 21:32:42 +1000

    refactor: rename STRAPI_URL env variable to API_URL
    - Replace STRAPI_URL with API_URL in environment config
    - Update both adminUrl and apiUrl references

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '$STRAPI_URL',
+  apiUrl: '$API_URL',
   dopplerConfig: '$DOPPLER_CONFIG',
   name: 'test',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: process.env['NODE_ENV'] == 'production',
-  adminUrl: process.env['STRAPI_URL'],
-  apiUrl: `${process.env['STRAPI_URL']}/api`,
+  adminUrl: process.env['API_URL'],
+  apiUrl: `${process.env['API_URL']}/api`,
   dopplerConfig: process.env['DOPPLER_CONFIG'],
   name: process.env['NODE_ENV'],
 };
