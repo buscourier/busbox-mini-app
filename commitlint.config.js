682a07e6ad498bd26db87395fea265707cbf1393
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-02-01 20:32:23 +1000

    chore: customize commitlint rules
    - Add explicit type enumeration (feat, fix, chore, docs, style, refactor, test)
    - Enforce lower-case type and sentence-case subject
    - Enforce non-empty type and subject

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,10 @@ module.exports = {
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'sentence-case']
+    'body-leading-blank': [2, 'always'],
+    'body-case': [2, 'always', 'sentence-case'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [2, 'always'],
+    'header-max-length': [2, 'always', 72]
   }
 };
