8aa37bb85fc8a468dcd155c088511a6d10b2abd4
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-02-09 23:12:06 +1000

    feat(delivery-details): integrate order management UI
    - Implemented dynamic order management with `app-order-tabs`
    - Integrated cargo type selection via `app-cargo-picker`
    - Added conditional rendering for different cargo types
      (`app-parcels`, `app-auto-parts`, `app-other-cargo`, `app-documents`)
    - Included packaging (`app-packaging`) and additional services
      (`app-additional-services`)
    - Optimized component imports and improved view model handling
    - Ensured async loading states with `tui-loader`

--- src/app/features/delivery/sub-features/delivery-details/delivery-details.component.ts
+++ src/app/features/delivery/sub-features/delivery-details/delivery-details.component.ts
@@ -1,20 +1,41 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
 import { Store } from '@ngrx/store';
 
-import { CargoType, Order, OrderValidationState } from '@features/delivery/types';
+import { TuiLoader } from '@taiga-ui/core';
 
+import { AdditionalServicesComponent } from './components/additional-services/additional-services.component';
+import { AutoPartsComponent } from './components/auto-parts/auto-parts.component';
+import { CargoPickerComponent } from './components/cargo-picker/cargo-picker.component';
+import { DocumentsComponent } from './components/documents/documents.component';
+import { OrderTabsComponent } from './components/order-tabs/order-tabs.component';
+import { OtherCargoComponent } from './components/other-cargo/other-cargo.component';
+import { PackagingComponent } from './components/packaging';
+import { ParcelsComponent } from './components/parcels/parcels.component';
 import { OrderActions } from './store/actions';
 import { deliveryDetailsFeature } from './store/feature';
 import { DeliveryDetailsViewModel } from './store/selectors/view-model.types';
+import { CargoType, Order, OrderValidationState } from './types';
 
 type OrderDataKeys = Exclude<keyof Order, 'id' | 'cargoType' | 'validation'>;
 
 @Component({
   selector: 'app-delivery-details',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    OrderTabsComponent,
+    CargoPickerComponent,
+    ParcelsComponent,
+    TuiLoader,
+    AutoPartsComponent,
+    OtherCargoComponent,
+    DocumentsComponent,
+    PackagingComponent,
+    AdditionalServicesComponent,
+  ],
   templateUrl: './delivery-details.component.html',
   styleUrl: './delivery-details.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -62,4 +83,6 @@ export class DeliveryDetailsComponent implements OnInit {
       }),
     );
   }
+
+  protected readonly CargoType = CargoType;
 }
