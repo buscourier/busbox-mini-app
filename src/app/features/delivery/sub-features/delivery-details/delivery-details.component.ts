2c944eb337131439d668bb6bd6259f11a7ba580b
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-02-09 16:28:58 +1000

    feat(delivery-details): add component implementation
    - Add store integration with view model subscription
    - Add methods for order management (add, remove, update)
    - Add max orders limit constant
    - Add type definition for order data keys
    - Implement order validation handling

--- src/app/features/delivery/sub-features/delivery-details/delivery-details.component.ts
+++ src/app/features/delivery/sub-features/delivery-details/delivery-details.component.ts
@@ -1,4 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { Store } from '@ngrx/store';
+
+import { CargoType, Order, OrderValidationState } from '@features/delivery/types';
+
+import { OrderActions } from './store/actions';
+import { deliveryDetailsFeature } from './store/feature';
+import { DeliveryDetailsViewModel } from './store/selectors/view-model.types';
+
+type OrderDataKeys = Exclude<keyof Order, 'id' | 'cargoType' | 'validation'>;
 
 @Component({
   selector: 'app-delivery-details',
@@ -7,4 +19,47 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './delivery-details.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DeliveryDetailsComponent {}
+export class DeliveryDetailsComponent implements OnInit {
+  private store = inject(Store);
+  protected readonly MAX_ORDERS = 4;
+
+  vm$!: Observable<DeliveryDetailsViewModel>;
+
+  ngOnInit(): void {
+    this.vm$ = this.store.select(deliveryDetailsFeature.selectViewModel);
+  }
+
+  addOrder(): void {
+    this.store.dispatch(OrderActions.add());
+  }
+
+  removeOrder(orderId: string): void {
+    this.store.dispatch(OrderActions.remove({ orderId }));
+  }
+
+  setActiveOrder(orderId: string): void {
+    this.store.dispatch(OrderActions.setActive({ orderId }));
+  }
+
+  setCargoType(orderId: string, cargoType: CargoType): void {
+    this.store.dispatch(OrderActions.setCargoType({ orderId, cargoType }));
+  }
+
+  updateOrderData(orderId: string, type: OrderDataKeys, data: Order[OrderDataKeys]): void {
+    this.store.dispatch(
+      OrderActions.updateData({
+        orderId,
+        data: { [type]: data },
+      }),
+    );
+  }
+
+  updateOrderValidation(orderId: string, type: keyof OrderValidationState, isValid: boolean) {
+    this.store.dispatch(
+      OrderActions.updateValidation({
+        orderId,
+        validation: { [type]: isValid },
+      }),
+    );
+  }
+}
