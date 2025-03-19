fc0ddfebb83bbb4aa784a5b084c8bdb12a4711da
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-03-19 20:33:45 +1000

    feat(delivery-point): export deliveryPointFeature from module index

--- src/app/features/delivery/delivery-point/index.ts
+++ src/app/features/delivery/delivery-point/index.ts
@@ -2,6 +2,7 @@ import { DeliveryPointActions } from './store';
 
 export * from './delivery-point.component';
 export * from './delivery-point.facade';
+export { deliveryPointFeature } from './store';
 
 /** Actions api  */
 export const selectDeliveryPointCity = DeliveryPointActions.selectCity;
